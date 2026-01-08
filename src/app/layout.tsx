7c59169f39ccc80a57d627db8f3ae5eb74c3e3ed
Eligi-Saas <suporte.eligi@gmail.com>
2026-01-08 02:32:25 -0300

    chore: remove estrutura antiga do repositório

--- src/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import AuthGuard from '../app/auth/AuthGuard';
-
-export default function AppLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <AuthGuard>{children}</AuthGuard>;
-}
