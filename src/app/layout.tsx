22df319197444eb70b3a9c31a8093dc5ce8d9b8d
Eligi-Saas <suporte.eligi@gmail.com>
2026-01-08 02:51:42 -0300

    chore: deploy update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import AuthGuard from '../app/auth/AuthGuard';
+import './globals.css'
 
-export default function AppLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <AuthGuard>{children}</AuthGuard>;
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
 }
