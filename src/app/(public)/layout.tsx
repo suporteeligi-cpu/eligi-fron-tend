1cada40df492c57f4e83da083fcae666d2cb5915
Eligi-Saas <suporte.eligi@gmail.com>
2026-01-16 19:39:12 -0300

    chore: deploy update

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,21 +1,19 @@
-import '@/styles/core.css';
 import '@/app/styles/glass.css';
 import '@/app/styles/landing.css';
+
 import Navbar from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
 
 export default function PublicLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR">
-      <body>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
   );
 }
