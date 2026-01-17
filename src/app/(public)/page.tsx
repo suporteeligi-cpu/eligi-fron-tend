6e476ccc2951b14ac2623fa7fc8418c67b2739ae
Eligi-Saas <suporte.eligi@gmail.com>
2026-01-16 23:57:41 -0300

    chore: deploy update

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useReveal } from '@/hooks/useReveal';
+
 import HeroSection from '../components/hero/HeroSection';
 import Ecosystem from '../components/sections/Ecosystem';
 import DashboardPreview from '../components/sections/DashboardPreview';
@@ -5,13 +9,27 @@ import Philosophy from '../components/sections/Philosophy';
 import FinalCTA from '../components/sections/FinalCTA';
 
 export default function HomePage() {
+  useReveal();
+
   return (
     <>
       <HeroSection />
-      <Ecosystem />
-      <DashboardPreview />
-      <Philosophy />
-      <FinalCTA />
+
+      <div className="reveal">
+        <Ecosystem />
+      </div>
+
+      <div className="reveal reveal-delay-1">
+        <DashboardPreview />
+      </div>
+
+      <div className="reveal reveal-delay-2">
+        <Philosophy />
+      </div>
+
+      <div className="reveal reveal-delay-3">
+        <FinalCTA />
+      </div>
     </>
   );
 }
