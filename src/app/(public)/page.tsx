fac8d29dd8f47a2de4da3d044331de29fdc39f1a
Eligi-Saas <suporte.eligi@gmail.com>
2026-02-04 23:32:35 -0300

    chore: deploy update

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -31,7 +31,6 @@ export default function HomePage() {
         <Statistics />
         <Ecosystem />
         <ProfessionalsSection />
-        <Philosophy />
         <DashboardPreview />
         <PricingSection />
         <FAQSection />
