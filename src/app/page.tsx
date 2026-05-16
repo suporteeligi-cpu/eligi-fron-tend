524ddd20d3aef315d48be8bf92dcb326146f49fb
Eligi-Saas <suporte.eligi@gmail.com>
2026-05-16 00:18:06 -0300

    chore: deploy update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,37 @@
-import { redirect } from 'next/navigation'
+'use client'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import Hero from './components/hero/HeroSection'
+import FloatingWhatsApp from './components/ui/FloatingWhatsApp'
+import AgendaSection from './components/sections/AgendaSection'
+import MetricsStrip from './components/sections/MetricsStrip'
+import Statistics from './components/sections/StatisticsSection'
+import DashboardPreview from './components/sections/DashboardPreview'
+import ProfessionalsSection from './components/sections/ProfessionalsSection'
+import Ecosystem from './components/sections/Ecosystem'
+import PricingSection from './components/sections/PricingSection'
+import FAQSection from './components/sections/FAQSection'
+import FinalCTA from './components/sections/FinalCTA'
+import styles from './(public)/Home.module.css'
 
 export default function RootPage() {
-  redirect('/(public)')
+  return (
+    <div className={styles.root}>
+      <Navbar />
+      <main className={styles.main}>
+        <Hero />
+        <AgendaSection />
+        <MetricsStrip />
+        <Statistics />
+        <Ecosystem />
+        <ProfessionalsSection />
+        <DashboardPreview />
+        <PricingSection />
+        <FAQSection />
+        <FinalCTA />
+        <FloatingWhatsApp />
+      </main>
+      <Footer />
+    </div>
+  )
 }
